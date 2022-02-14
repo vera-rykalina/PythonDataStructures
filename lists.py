d94c39629bd0f8897ab245e98c8b62bba2360f4c
Vera Rykalina <verarykalina@gmail.com>
2022-02-14 12:12:55 +0100

    Added some code

--- lists.py
+++ lists.py
@@ -18,7 +18,72 @@
 #     print("No requirment is fullfilled!")
 
 # Lists
+# weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday",  "Friday", "Saturday", "Sunday"]
+# print(weekdays)
+# print(weekdays[3])
+# print(weekdays[3]  + " " +  " " + weekdays[2])
 
-weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday",  "Friday", "Saturday", "Sunday"]
-print(weekdays)
-print(weekdays[3])
+# Exercise
+'''
+Write a program that creates at least four different lists
+'''
+# StringList = ["string1", "string2", "string3"]
+# print(StringList)
+# NumberList = [3,  5,  7]
+# print(NumberList)
+# MixedList = [3, "string1", 5, "string2", True]
+# print(MixedList)
+# MixedList = StringList + NumberList + MixedList
+# print(MixedList)
+
+
+# Range function
+# RangeList1 = list(range(4))
+# print(RangeList1)
+# RangeList2 = list(range(4,9))
+# print(RangeList2)
+# RangeList3 = list(range(4,10,2))
+# print(RangeList3)
+
+# Exersice
+# number1 = int(input("Type a length number: "))
+# List1 = list(range(number1))
+# print(List1)
+# print(len(List1))
+#
+# number2 = int(input("Type a length number: "))
+# List2 = list(range(1, number2))
+# print(List2)
+# print(len(List2))
+#
+# number3 = int(input("Type a length number: "))
+# List3 = list(range(2, number3, 2))
+# print(List3)
+# print(len(List3))
+
+# Exeercise
+# number = int(input("Type a number : "))
+# List1 = list(range(1, number))
+# print(List1)
+# number = int(input("Type a number : "))
+# List2 = list(range(2, number, 2))
+# print(List2)
+# number = int(input("Type a number : "))
+# List3 = list(range(3, number, 3))
+# print(List3)
+
+# List functions
+import random
+mylist =[1,3,10,5,2]
+print(mylist)
+mylist.sort()
+print(mylist)
+
+mylist.append(222)
+print(mylist)
+mylist.pop()
+print(mylist)
+mylist.reverse()
+print(mylist)
+random.shuffle(mylist)
+print(mylist)
